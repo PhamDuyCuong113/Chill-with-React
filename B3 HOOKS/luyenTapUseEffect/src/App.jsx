a2ec67e10983ea40dc2aa19dc48d837989343ab6
PhamDuyCuong113 <phamduycuong2005241@gmail.com>
2026-03-19 10:31:13 +0700

    them effect

--- B3 HOOKS/luyenTapUseEffect/src/App.jsx
+++ B3 HOOKS/luyenTapUseEffect/src/App.jsx
@@ -1,19 +1,53 @@
 import { useState, useEffect} from 'react'
+import Content from './Content.jsx'
 
+const tabs = ["posts", "comments", "users"];
 
 function App() {
-  const [title, setTitle] = useState('');
-
+    const [title, setTitle] = useState("");
+    const [show, setShow] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [type, setType] = useState("users");
+    
     useEffect(() => {
-     console.log('Mounted', title);
-    })
-    return (
-        <div className='App'>  
-            <input 
-                value={title} 
-                onChange={e => setTitle(e.target.value)}
-            />
+        document.title = title;
+        console.log({type});
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        .then(res => res.json())
+        .then(data => setUsers(data));
+    }, [type]);
 
+    const handleShow    = () => {
+        setShow(!show);
+    } 
+    const handleChange = (e) => {
+        setTitle(e.target.value);
+    }
+
+    return (
+        <div >  
+            <button onClick={handleShow}>SHOW</button>
+            {show && <Content />}
+            <input type="text" value={title} onChange={handleChange} placeholder="Enter title..." />
+            {tabs.map(tab => (
+                <button 
+                    key={tab} 
+                    style={type === tab ? {
+                        color : '#fff',
+                        backgroundColor : '#333'
+                    } : {}
+                    }
+                    onClick={() => setType(tab)}>
+                
+                    {tab}
+                </button>
+            ))}]
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.id} : {user.name || user.title}</li>
+                ))}
+            </ul>
+            
         </div>
     );
 }
---
+++ B3 HOOKS/luyenTapUseEffect/src/Content.jsx
@@ -0,0 +1,5 @@
+
+function Content(){
+    return <h1>Pham Duy Cuong</h1>
+}
+export default Content
